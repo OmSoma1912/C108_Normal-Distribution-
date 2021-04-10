2ae55eeffff7ab4dadd8afc68df2adc0d537eb70
OmSoma1912 <61110267+OmSoma1912@users.noreply.github.com>
2021-04-10 16:36:24 +0530

    Add files via upload

---
+++ C108/code.py
@@ -0,0 +1,13 @@
+import random
+import plotly.figure_factory as ff
+
+dice_result = []
+for i in range (1,100):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice_result.append(dice1 + dice2)
+
+fig = ff.create_distplot([dice_result], ["Result"])
+fig.show()
+
+
